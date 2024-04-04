20a94588d8e9108194514e751409024885a28610
jgarridoy <jgarridoy@miumg.edu.gt>
2024-04-03 23:19:33 -0600

    Initial commit

---
+++ src/main/java/org/example/LinkedListCycle.java
@@ -0,0 +1,51 @@
+package org.example;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        ListNode head = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(0);
+        ListNode node3 = new ListNode(-4);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1; // Creando un ciclo
+
+        LinkedListCycle solution = new LinkedListCycle();
+        boolean hasCycle = solution.hasCycle(head);
+        System.out.println("¿La lista enlazada tiene un ciclo? " + hasCycle); // Output: true
+    }
+}
